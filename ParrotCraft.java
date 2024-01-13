dd03c44b892f5ac19025425564fab1911d44a1ce
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-01-12 23:00:53 -0500

    Update ParrotCraft.java

--- ParrotCraft.java
+++ ParrotCraft.java
@@ -1,26 +1,39 @@
-//@Author: Sujan Rokad, 000882948
+/**
+ * Author:                 Sujan Rokad, 000882948
+ * Authorship statement:   I, Sujan Rokad, 000882948, certify that this material is my original work.
+ *                         No other person's work has been used without due acknowledgement.
+ * Purpose:                Simulate a ParrotCraft program where users can interact with parrots.
+ */
 
-import java.util.*;
+import java.util.Scanner;
 
 public class ParrotCraft {
+
+    /**
+     * The main method of the ParrotCraft program.
+     *
+     * @param args The command-line arguments (unused).
+     */
     public static void main(String[] args) {
-//        getting inputs from user to name parrots and defining three new parrots p1, p2, p3
         Scanner input = new Scanner(System.in);
+
+        // Creating three parrots and getting names from the user
         Parrot p1 = new Parrot();
         Parrot p2 = new Parrot();
         Parrot p3 = new Parrot();
+
         System.out.println("Enter name for Parrot 1");
         String parrot1 = input.next();
         System.out.println("Enter name for Parrot 2");
         String parrot2 = input.next();
         System.out.println("Enter name for Parrot 3");
         String parrot3 = input.next();
+
         p1.setName(parrot1);
         p2.setName(parrot2);
         p3.setName(parrot3);
 
-//        While loop for outputs and keep running the code
-
+        // Interaction loop with the parrots
         while (true) {
             System.out.println(p1.toString());
             System.out.println(p2.toString());
@@ -28,32 +41,26 @@ public class ParrotCraft {
             System.out.println("1.FEED 2.COMMAND 3.PLAY 4.HIT 5.QUIT");
             System.out.println("Select one option from above");
 
-//        Creating option variable to save input for choice of interface from the menu
-
             int option;
             option = input.nextInt();
             if (option == 5)
                 break;
+
             System.out.println("Which Parrot?");
             int p = input.nextInt();
 
-//        Introducing new variable called selectedParrot to save value of user's selected choice of parrot
-//        using switch method to rerun all the method
-
-            Parrot selectedParrot = new Parrot();
+            Parrot selectedParrot;
             switch (p) {
                 case 1: selectedParrot = p1;
-                break;
+                        break;
                 case 2: selectedParrot = p2;
-                break;
+                        break;
                 case 3: selectedParrot = p3;
-                break;
+                        break;
                 default:
-                    System.out.println("Choose valid Parrot");
+                    System.out.println("Choose a valid Parrot");
                     continue;
             }
-
-//            4 switch cases for 4 type of menu interface (feed, play, hit, command)
 
             switch (option) {
                 case 1:
@@ -67,23 +74,19 @@ public class ParrotCraft {
                     System.out.println(selectedParrot.command(flying));
                     break;
                 case 3:
-                    if (selectedParrot.alive_dead == "dead") {
-                        System.out.println("A dead parrot cannot feed");
-                    }
-                    else {
+                    if ("dead".equals(selectedParrot.alive_dead)) {
+                        System.out.println("A dead parrot cannot play");
+                    } else {
                         System.out.println("Which parrot do you want to play");
                         int parrotPlay = input.nextInt();
-                        if (parrotPlay==p) {
+                        if (parrotPlay == p) {
                             System.out.println("Parrot cannot play with itself");
                             continue;
-                        }
-                        else if (parrotPlay==1) {
+                        } else if (parrotPlay == 1) {
                             System.out.println(selectedParrot.play(p1));
-                        }
-                        else if (parrotPlay==2){
+                        } else if (parrotPlay == 2) {
                             System.out.println(selectedParrot.play(p2));
-                        }
-                        else if (parrotPlay==3) {
+                        } else if (parrotPlay == 3) {
                             System.out.println(selectedParrot.play(p3));
                         }
                     }
@@ -92,12 +95,8 @@ public class ParrotCraft {
                     System.out.println(selectedParrot.hit());
                     break;
                 default:
-                    System.out.println("Select valid choice");
+                    System.out.println("Select a valid choice");
             }
         }
-
-
     }
-
-
 }
